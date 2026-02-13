3f24ce82543506b398b4ea02ea5910a77ec360ae
Jesús Alexander Herrera Acevedo <152633538+ExplosiveSmile05@users.noreply.github.com>
2026-02-13 12:08:49 -0600

    Fix import path for Component componenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Component from './Components/Component.jsx'
+import Component from './components/Component.jsx'
 import './App.css'
 
 function App() {
